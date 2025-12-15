cc75b151de0d413b8836a8c825dc18d21438585e
Aditya Kumar <adityapal1409@gmail.com>
2025-12-15 06:29:24 +0000

    init: project initialise

---
+++ cmd/server/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+    "log"
+
+    "github.com/gofiber/fiber/v2"
+)
+
+
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+	
+
+	log.Fatal(app.Listen(":3000"))
+	log.Println("Server is running on http://localhost:3000")
+}					
